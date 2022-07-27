e4d8dbd5e48b9b3f706fed9ae36d02108678ce22
LAXMIKANT SARASWAT <laxmikantsarswat007@gmail.com>
2022-07-27 09:33:01 +0530

    my 1st commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import "./App.css";
+
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+export default class App extends Component {
+  static propTypes = {
+    prop: PropTypes,
+  };
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+
+          <Switch>
+            <Route exact path="/">
+              {" "}
+              <News
+                key="science"
+                pageSize={6}
+                country="in"
+                category="science"
+              />{" "}
+            </Route>
+            <Route exact path="/entertainment">
+              {" "}
+              <News
+                key="entertainment"
+                pageSize={6}
+                country="in"
+                category="entertainment"
+              />{" "}
+            </Route>
+            <Route exact path="/general">
+              {" "}
+              <News
+                key="general"
+                pageSize={6}
+                country="in"
+                category="general"
+              />{" "}
+            </Route>
+            <Route exact path="/business">
+              {" "}
+              <News
+                key="business"
+                pageSize={6}
+                country="in"
+                category="business"
+              />{" "}
+            </Route>
+            <Route exact path="/health">
+              {" "}
+              <News
+                key="health"
+                pageSize={6}
+                country="in"
+                category="health"
+              />{" "}
+            </Route>
+            <Route exact path="/science">
+              {" "}
+              <News
+                key="science"
+                pageSize={6}
+                country="in"
+                category="science"
+              />{" "}
+            </Route>
+            <Route exact path="/sports">
+              {" "}
+              <News
+                key="sports"
+                pageSize={6}
+                country="in"
+                category="sports"
+              />{" "}
+            </Route>
+            <Route exact path="/technology">
+              {" "}
+              <News
+                key="technology"
+                pageSize={6}
+                country="in"
+                category="technology"
+              />{" "}
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
